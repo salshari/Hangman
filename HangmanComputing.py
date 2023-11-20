f90c48add3a0dd01e1258afae0a6c0116ba613ce
shyma <alshari.s@northeastern.edu>
2023-11-20 14:23:26 -0500

    Define Colors

--- HangmanComputing.py
+++ HangmanComputing.py
@@ -13,3 +13,7 @@ height = 500
 # initialize the Pygame window to specified dimensions
 # this screen is where the Hangman figure will be drawn if the Pygame option is chosen by user as display
 screen = pygame.display.set_mode((width, height))
+
+# use RGB values to define colors 
+black = (0, 0, 0)
+white = (255, 255, 255)
