e2b02e7774e1d90c6860e0c5f4de8246acd08a19
shyma <alshari.s@northeastern.edu>
2023-11-21 15:43:19 -0500

    Create function to draw hangman figure in pygame window

--- HangmanComputing.py
+++ HangmanComputing.py
@@ -67,3 +67,44 @@ def draw_gallow_in_pygame():
 
     # update the Pygame window
     pygame.display.update()
+
+def draw_hangman_pygame(stage):
+    '''
+    determines which parts of the Hangman figure will be 
+    drawn to the Pygame window using the current stage
+    '''
+
+    # set the background color of the Pygame window
+    screen.fill(white)
+    
+    # after screen has been filled white, call the following function to draw the gallow in the pygame window
+    draw_gallow_in_pygame()
+
+    # set up the stages that the Hangman figure will be drawn in to the Pygame window
+    
+    # if stage greater than or equal to 1, draw the head of the Hangman figure
+    if stage >= 1:
+        pygame.draw.circle(screen, black, (250, 100), 20)
+
+    # if stage greater than or equal to 2, draw the body of the Hangman figure
+    if stage >= 2:
+        pygame.draw.line(screen, black, (250, 120), (250, 200), 5)
+
+    # if stage greater than or equal to 3, draw the left arm of the Hangman figure
+    if stage >= 3:
+        pygame.draw.line(screen, black, (250, 140), (200, 170), 5)
+
+    # if stage greater than or equal to 4, draw the right arm of the Hangman figure
+    if stage >= 4:
+        pygame.draw.line(screen, black, (250, 140), (300, 170), 5)
+
+    # if stage greater than or equal to 5, draw the left leg of the Hangman figure
+    if stage >= 5:
+        pygame.draw.line(screen, black, (250, 200), (200, 250), 5)
+
+    # if stage greater than or equal to 6, draw the right leg of the Hangman figure
+    if stage >= 6:
+        pygame.draw.line(screen, black, (250, 200), (300, 250), 5)
+
+    # update the Pygame window
+    pygame.display.flip()
